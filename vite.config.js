b21aeb10754b5c25222f5bc431b92ab5cb6eeeca
Xinjian-Zhang <1xinjian.zhang@gmail.com>
2025-07-08 00:39:27 +0300

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
-  base: '/efloa-web/', 
+  base: '/', 
   plugins: [react(), WindiCSS()],
 })
